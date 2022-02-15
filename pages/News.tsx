f7e86ae9e68a19772c84cfd233b5882f8b29e904
takoyan33 <harrier2070@gmail.com>
2022-02-15 10:50:23 +0900

    fix page

--- pages/News.tsx
+++ pages/News.tsx
@@ -1,16 +1,15 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-export default function Portfolio() {
+export default function News() {
   return (
     <>
       <Header />
 
-      <h1>Portfolio</h1>
+      <h1>News</h1>
       <p>準備中・・・</p>
 
       <Footer />
     </>
   );
 }
-
