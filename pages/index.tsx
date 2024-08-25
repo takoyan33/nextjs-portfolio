a325594abf281fe07a75a1d89f7436a72ef199f4
abeshumpei <harrier2070@gmail.com>
2024-08-25 16:16:03 +0900

    feat: 戻るボタンの追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,22 @@ import Head from 'next/head'
 import Footer from '../components/Components/ui/Footer'
 import Header from '../components/Components/ui/Header'
 import Main from '../components/Main'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Script from 'next/script'
+import Image from 'next/image'
 // import MoonLoader from 'react-spinners/MoonLoader'
 
 export default function Home() {
+  const [showBackButton, setShowBackButton] = useState(false)
+
+  const handleScroll = () => {
+    setShowBackButton(window.scrollY > 150)
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
   return (
     <div className='container'>
       <Head>
@@ -23,6 +34,16 @@ export default function Home() {
       <Header />
       {/* <MoonLoader /> */}
       <Main />
+      {showBackButton && (
+        <button
+          className='back__btn'
+          onClick={() => {
+            window.scrollTo(0, 0)
+          }}
+        >
+          <Image src='/images/top-arrow.svg' height={30} width={30} alt='arrow' />
+        </button>
+      )}
       <Footer />
     </div>
   )
