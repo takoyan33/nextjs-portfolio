9ec69dc0e4b6ef1da927628ee7f7df8be963ee02
abeshumpei <harrier2070@gmail.com>
2024-10-08 19:24:11 +0900

    fix:問い合わせの修正

--- components/Components/ui/ZennArticleItem.tsx
+++ components/Components/ui/ZennArticleItem.tsx
@@ -21,7 +21,7 @@ export default function ZennArticleItem({
   const formatDate = (dateString) => {
     const date = new Date(dateString)
     const year = date.getFullYear()
-    const month = String(date.getMonth() + 1).padStart(2, '0') // 月は0ベースなので+1
+    const month = String(date.getMonth() + 1).padStart(2, '0')
     const day = String(date.getDate()).padStart(2, '0')
 
     return `${year}-${month}-${day}`
@@ -31,10 +31,10 @@ export default function ZennArticleItem({
       <Link href={`https://zenn.dev${zenn_path}`} target='_blank'>
         <div className='zenn__emoji'>{zenn_emoji}</div>
         <div className='zenn__flex'>
-          <p className='zenn__date'>{formatDate(zenn_published_at)}</p>
           <li className='zenn__tag'>{zenn_article_type}</li>
         </div>
         <p className='zenn__title'> {zenn_title} </p>
+        <p className='zenn__date'>{formatDate(zenn_published_at)}</p>
       </Link>
     </div>
   )
