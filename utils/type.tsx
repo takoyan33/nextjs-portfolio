c4eeb802d7e9af0d2ff7d2fbf4660b71aaea9146
abeshumpei <harrier2070@gmail.com>
2024-11-10 11:04:20 +0900

    fix: portfolioの詳細が見れるように修正

--- utils/type.tsx
+++ utils/type.tsx
@@ -1,96 +1,96 @@
 export type history = {
-  title: string
-  date: string
-  body: string
+	title: string
+	date: string
+	body: string
 }
 
 export type historyProps = {
-  history: history[]
+	history: history[]
 }
 
 export type job = {
-  title: string
-  date: string
-  body: string
+	title: string
+	date: string
+	body: string
 }
 
 export type jobProps = {
-  job: job[]
+	job: job[]
 }
 
 export type license = {
-  date: string
-  title: string
+	date: string
+	title: string
 }
 
 export type licenseProps = {
-  license: license[]
+	license: license[]
 }
 
 export type skill = {
-  name: string
-  rank: string
-  tag: string
-  about: string
-  icon: string
+	name: string
+	rank: string
+	tag: string
+	about: string
+	icon: string
 }
 
 export type skillProps = {
-  skill: skill[]
+	skill: skill[]
 }
 
 export type portfolioType = {
-  id: number
-  name: string
-  date: string
-  tag: string[]
-  topImg: string
-  front_url: string
-  back_url: string
-  front_github: string
-  back_github: string
-  color: string
-  about: string
-  aboutImg: string
-  function: string
-  functionImg: string
-  appeal: string
-  appealImg: string
-  front_skill: string[]
-  back_skill: string[]
-  infra_skill: string[]
-  time: string
+	id: string
+	name: string
+	date: string
+	tag: string[]
+	topImg: string
+	front_url: string
+	back_url: string
+	front_github: string
+	back_github: string
+	color: string
+	about: string
+	aboutImg: string
+	function: string
+	functionImg: string
+	appeal: string
+	appealImg: string
+	front_skill: string[]
+	back_skill: string[]
+	infra_skill: string[]
+	time: string
 }
 
 export type PortfolioProps = {
-  portfolio: portfolioType[]
+	portfolio: portfolioType[]
 }
 
 export type zenn = {
-  id: number
-  post_type: 'Article'
-  title: string
-  slug: string
-  comments_count: number
-  liked_count: number
-  body_letters_count: number
-  article_type: 'tech'
-  emoji: string
-  is_suspending_private: boolean
-  published_at: Date
-  body_updated_at: string
-  source_repo_updated_at: null | string
-  pinned: boolean
-  path: string
-  user: {
-    id: number
-    username: string
-    name: string
-    avatar_small_url: string
-  }
-  publication: null
+	id: number
+	post_type: "Article"
+	title: string
+	slug: string
+	comments_count: number
+	liked_count: number
+	body_letters_count: number
+	article_type: "tech"
+	emoji: string
+	is_suspending_private: boolean
+	published_at: Date
+	body_updated_at: string
+	source_repo_updated_at: null | string
+	pinned: boolean
+	path: string
+	user: {
+		id: number
+		username: string
+		name: string
+		avatar_small_url: string
+	}
+	publication: null
 }
 
 export type zennProps = {
-  articles: zenn[]
+	articles: zenn[]
 }
