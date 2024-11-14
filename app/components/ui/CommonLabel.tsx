a79cfa7672df929b756cfe9e7871ab5a9b955db1
abeshumpei <harrier2070@gmail.com>
2024-11-14 19:05:38 +0900

    fix: slideのhoverの追加

---
+++ app/components/ui/CommonLabel.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link"
+import type React from "react"
+
+interface CommonLabelProps {
+	text: string
+	link?: string
+	className?: string
+	handleClick?: () => void
+}
+
+export const CommonLabel: React.FC<CommonLabelProps> = ({
+	text,
+	className = "more",
+	handleClick,
+	link,
+}) => {
+	return (
+		<Link href={link ? link : ""}>
+			<button
+				type="button"
+				onClick={handleClick}
+				className={`main__btn ${className}`}
+				aria-label="送信"
+			>
+				{text}
+			</button>
+		</Link>
+	)
+}
