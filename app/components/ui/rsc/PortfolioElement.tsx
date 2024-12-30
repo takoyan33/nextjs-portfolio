23d1c15994686371e3d64ac67e54417535134faf
abeshumpei <harrier2070@gmail.com>
2024-12-30 21:42:11 +0900

    fix: portfoliosを修正

--- app/components/ui/rsc/PortfolioElement.tsx
+++ app/components/ui/rsc/PortfolioElement.tsx
@@ -1,21 +1,34 @@
-import React from "react"
+"use client"
+import React, { useEffect, useState } from "react"
 import { fetchPortfolios } from "../../../../hooks/fetch"
 import type { portfolioType } from "../../../../utils/type"
 // import portfolios from "../../../../api/portfolios/index.json"
 import PortfolioItem from "../PortfolioItem"
 
 export const PortfolioElement = async () => {
-	const portfolios = await fetchPortfolios()
+	//const portfolios = await fetchPortfolios()
+
+	const [portfolios, setPortfolios] = useState<portfolioType[]>()
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const data = await fetchPortfolios()
+			setPortfolios(data.data)
+		}
+
+		fetchData()
+	}, [])
+
 	return (
 		<div className="max_width">
 			<h3 className="portfolio__headTitle">
 				全ての制作物
 				<span className="portfolio__headTitle-span">
-					{portfolios?.data?.length}件
+					{portfolios?.length}件
 				</span>
 			</h3>
 			<div className="portfolioFlx padding">
-				{portfolios?.data?.map((portfolio: portfolioType) => (
+				{portfolios?.map((portfolio: portfolioType) => (
 					<PortfolioItem
 						key={portfolio.id}
 						portfolio_id={portfolio.id}
