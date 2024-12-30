587f00b907eeb7123b615eeffd4c51a1215a1381
abeshumpei <harrier2070@gmail.com>
2024-12-30 19:36:53 +0900

    add: test,historiesを取得

--- next.config.js
+++ next.config.js
@@ -13,5 +13,9 @@ module.exports = {
 			process.env.NODE_ENV === "development" ? "pbadl6xwcf" : "pbadl6xwcf",
 		NEXT_PUBLIC_GATAG:
 			process.env.NODE_ENV === "development" ? "G-R47KRGK42T" : "G-R47KRGK42T",
+		NEXT_PUBLIC_BACKEND_API_URL:
+			process.env.NODE_ENV === "development"
+				? "https://to-you-design-api.onrender.com/"
+				: "https://to-you-design-api.onrender.com/",
 	},
 }
