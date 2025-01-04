e046d6bea232c33f9ba7b85d812a71c177406bc6
abeshumpei <harrier2070@gmail.com>
2025-01-04 18:37:43 +0900

    fix: abouttabをコンポーネントか

--- app/components/ui/rsc/License.tsx
+++ app/components/ui/rsc/License.tsx
@@ -3,27 +3,28 @@
 import React, { useEffect, useState } from "react"
 import { fetchLicenses } from "../../../../hooks/fetch"
 //import licenses from "../../../../api/licenses/index.json"
-import type { license } from "../../../../utils/type"
+// import type { license } from "../../../../utils/type"
 
-export const License = () => {
-	const [licenses, setLicenses] = useState<license[]>([])
-	useEffect(() => {
-		const fetchData = async () => {
-			const data = await fetchLicenses()
-			setLicenses(data.data)
-		}
+export const License = async () => {
+	const licenses = await fetchLicenses()
 
-		fetchData()
-	}, [])
+	// const [licenses, setLicenses] = useState<license[]>([])
+	// useEffect(() => {
+	// 	const fetchData = async () => {
+	// 		const data = await fetchLicenses()
+	// 		setLicenses(data.data)
+	// 	}
+
+	// 	fetchData()
+	// }, [])
 	return (
 		<>
-			{Array.isArray(licenses) &&
-				licenses.map((license) => (
-					<tr key={license.id} className="license__table-tr">
-						<td className="license__table-td">{license.date}</td>
-						<td className="license__table-td">{license.title}</td>
-					</tr>
-				))}
+			{licenses?.data.map((license) => (
+				<tr key={license.id} className="license__table-tr">
+					<td className="license__table-td">{license.date}</td>
+					<td className="license__table-td">{license.title}</td>
+				</tr>
+			))}
 		</>
 	)
 }
