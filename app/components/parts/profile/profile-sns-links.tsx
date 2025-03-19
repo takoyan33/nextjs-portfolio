187371ec0a84af635c358dc3e6fa3d1c7ec54f8a
abeshumpei <harrier2070@gmail.com>
2025-03-19 20:02:27 +0900

    update: margin-block, padding-blockの利用

--- app/components/parts/profile/profile-sns-links.tsx
+++ app/components/parts/profile/profile-sns-links.tsx
@@ -4,17 +4,12 @@ import { SOCIAL_LINKS } from "utils/data"
 
 export const ProfileSnsLinks = () => {
 	return (
-		<div className="about__flx">
+		<div className="about__container">
 			{SOCIAL_LINKS.map((link) => (
-				<Link
-					key={link.href}
-					href={link.href}
-					target="_blank"
-					rel="noopener noreferrer"
-				>
+				<Link key={link.href} href={link.href} target="_blank">
 					<Image
 						src={link.src}
-						className="about_snsLogo"
+						className="about__sns-icon"
 						alt={link.alt}
 						height={link.height}
 						width={link.width}
