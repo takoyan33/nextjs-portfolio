33b8f52a38213f502a774e985cfd93d66673a56e
abeshumpei <harrier2070@gmail.com>
2025-03-27 00:15:36 +0900

    update: portfolio/idのパンくずをコンポーネント化

--- app/components/ui/breadcrumb.tsx
+++ app/components/ui/breadcrumb.tsx
@@ -6,9 +6,14 @@ import styles from "./css/breadcrumb.module.scss"
 interface BreadcrumbProps {
 	name: string
 	link: string
+	thirdTitle?: string
 }
 
-export const Breadcrumb: React.FC<BreadcrumbProps> = ({ name, link }) => {
+export const Breadcrumb: React.FC<BreadcrumbProps> = ({
+	name,
+	link,
+	thirdTitle = "",
+}) => {
 	return (
 		<p className={`${styles.breadcrumb__title}`}>
 			<TransitionLink href={PATH.INDEX} className={styles.breadCrumb__first}>
@@ -25,6 +30,20 @@ export const Breadcrumb: React.FC<BreadcrumbProps> = ({ name, link }) => {
 			<TransitionLink href={link} className={styles.breadCrumb__second}>
 				{name}
 			</TransitionLink>
+			{thirdTitle && (
+				<>
+					<span className={styles.breadcrumb__arrow}>
+						<Image
+							src="/images/next-arrow.svg"
+							width={15}
+							height={15}
+							priority
+							alt="スライドショーのナビゲーション"
+						/>
+					</span>
+					<span className={styles.breadcrumb__second}>{thirdTitle}</span>
+				</>
+			)}
 		</p>
 	)
 }
