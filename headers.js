6a9e3a08e521ce2398a36a652fd8a564924f1028
abeshumpei <harrier2070@gmail.com>
2025-03-30 18:14:26 +0900

    update: next.jsのセキュリティ向上

---
+++ headers.js
@@ -0,0 +1,35 @@
+module.exports = [
+	{
+		key: "X-DNS-Prefetch-Control",
+		value: "on",
+	},
+	{
+		key: "Strict-Transport-Security",
+		value: "max-age=63072000; includeSubDomains; preload",
+	},
+	// todo: Server が決まってる場合は設定できる
+	// {
+	//     key: 'Server',
+	//     value: 'Apache', // phony server value
+	// },
+	{
+		key: "X-Content-Type-Options",
+		value: "nosniff",
+	},
+	{
+		key: "X-Frame-Options",
+		value: "sameorigin",
+	},
+	{
+		key: "X-XSS-Protection",
+		value: "1; mode=block",
+	},
+	{
+		key: "Referrer-Policy",
+		value: "same-origin",
+	},
+	{
+		key: "Permissions-Policy",
+		value: "camera=*",
+	},
+]
