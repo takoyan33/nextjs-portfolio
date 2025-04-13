deaf133011f89b6652048639b53de98c67527f9c
abeshumpei <harrier2070@gmail.com>
2025-04-13 09:59:41 +0900

    add: プライバシーポリシーページの作成

--- utils/path.ts
+++ utils/path.ts
@@ -1,7 +1,10 @@
+import PrivacyPolicy from "app/privacy-policy/page";
+
 export const PATH = {
   INDEX: '/',
   ABOUT: '/about',
   PORTFOLIO: '/portfolios',
   BLOG: '/blog',
   CONTACT: '/contact',
+  PRIVACY_POLICY: '/privacy-policy'
 } as const
