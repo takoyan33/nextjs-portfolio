3a57849d175138a6a9db91fb15a1b020a650764a
shun <59337731+takoyan33@users.noreply.github.com>
2025-05-12 21:59:09 +0900

    update: clarityの修正  (#108)
    * update: 関数の短縮化
    
    * test:
    
    * envテスト
    
    * update: clarityの修正

--- utils/clarity.tsx
+++ utils/clarity.tsx
@@ -1,7 +1,5 @@
 import Script from "next/script"
 
-const CLARITY_ID = process.env.NEXT_PUBLIC_CLARITY_ID
-
 export function Clarity() {
 	return (
 		<Script strategy="afterInteractive" id="clarity-tag">
@@ -10,7 +8,7 @@ export function Clarity() {
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "${CLARITY_ID}");
+    })(window, document, "clarity", "script", "pbadl6xwcf");
     `}
 		</Script>
 	)
