f1b10d497b2d716cae42486afec0cb0044392ed4
shun <59337731+takoyan33@users.noreply.github.com>
2025-06-27 21:47:48 +0900

    メンテナンス画面の追加 (#115)
    * update: input, css修正
    
    * test: vercel edge-config
    
    * test: vercel edge-config メンテナンス画面の追加

--- middleware.js
+++ middleware.js
@@ -1,11 +1,15 @@
 import { get } from "@vercel/edge-config"
 import { NextResponse } from "next/server"
 
-export const config = { matcher: "/welcome" }
+export async function middleware(request) {
+	const isInMaintenanceMode = await get("isMaintenance")
 
-export async function middleware() {
-	const greeting = await get("greeting")
-	// NextResponse.json requires at least Next v13.1 or
-	// enabling experimental.allowMiddlewareResponseBody in next.config.js
-	return NextResponse.json(greeting)
+	// メンテナンスモード中なら /maintenance にリライト
+	if (isInMaintenanceMode) {
+		const url = request.nextUrl.clone()
+		url.pathname = "/maintenance"
+		return NextResponse.rewrite(url)
+	}
+	// 通常どおりリクエストを続ける
+	return NextResponse.next()
 }
