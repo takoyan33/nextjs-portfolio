1dfc8bfc035f56e02dd9ea6206dea413180c17b2
abeshumpei <harrier2070@gmail.com>
2025-08-02 15:16:09 +0900

    upadate: yarn lintの実行

--- app/(home)/_containers/home-skills.tsx
+++ app/(home)/_containers/home-skills.tsx
@@ -1,54 +1,52 @@
-"use client"
+'use client'
 
-import ScrollComponent from "hooks/use-fadeIn"
-import React from "react"
-import useSWR from "swr"
+import ScrollComponent from 'hooks/use-fadeIn'
+import useSWR from 'swr'
 import {
-	fetchBackSkills,
-	fetchFrontSkills,
-	fetchInfraSkills,
-	fetchOtherSkills,
-} from "../../../hooks/fetch"
-import type { Skill } from "../../../types"
-import { SkillElement } from "../../components/ui/skill-element"
+  fetchBackSkills,
+  fetchFrontSkills,
+  fetchInfraSkills,
+  fetchOtherSkills,
+} from '../../../hooks/fetch'
+import { SkillElement } from '../../components/ui/skill-element'
 
 const fetchSkillData = async () => {
-	const [front, back, infra, other] = await Promise.all([
-		fetchFrontSkills(),
-		fetchBackSkills(),
-		fetchInfraSkills(),
-		fetchOtherSkills(),
-	])
+  const [front, back, infra, other] = await Promise.all([
+    fetchFrontSkills(),
+    fetchBackSkills(),
+    fetchInfraSkills(),
+    fetchOtherSkills(),
+  ])
 
-	return {
-		Frontend: front.data,
-		Backend: back.data,
-		Infra: infra.data,
-		Other: other.data,
-	}
+  return {
+    Frontend: front.data,
+    Backend: back.data,
+    Infra: infra.data,
+    Other: other.data,
+  }
 }
 
 export const HomeSkills = () => {
-	const { data: skills, error, isLoading } = useSWR("skills", fetchSkillData)
+  const { data: skills, error, isLoading } = useSWR('skills', fetchSkillData)
 
-	if (isLoading) return <div>Loading...</div>
-	if (error) return <div>エラーが発生しました</div>
-	if (!skills) return null
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>エラーが発生しました</div>
+  if (!skills) return null
 
-	return (
-		<div>
-			{Object.entries(skills).map(([title, skillList]) => (
-				<div key={title}>
-					<h3 className="skill__title">{title}</h3>
-					<ScrollComponent>
-						<ul className="skill__container">
-							{skillList.map((skill) => (
-								<SkillElement key={skill.name + skill.id} {...skill} />
-							))}
-						</ul>
-					</ScrollComponent>
-				</div>
-			))}
-		</div>
-	)
+  return (
+    <div>
+      {Object.entries(skills).map(([title, skillList]) => (
+        <div key={title}>
+          <h3 className='skill__title'>{title}</h3>
+          <ScrollComponent>
+            <ul className='skill__container'>
+              {skillList.map((skill) => (
+                <SkillElement key={skill.name + skill.id} {...skill} />
+              ))}
+            </ul>
+          </ScrollComponent>
+        </div>
+      ))}
+    </div>
+  )
 }
