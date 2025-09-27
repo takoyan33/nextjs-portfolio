9014fc1322e48d506cc5a90c175b680ccad2ceed
abeshumpei <harrier2070@gmail.com>
2025-09-27 23:46:25 +0900

    fix: testの修正とtestカバレッジの追加

--- __tests__/unit/lower-title.test.tsx
+++ __tests__/unit/lower-title.test.tsx
@@ -1,7 +1,8 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { test } from "vitest"
 import { LowerTitle } from "../../app/components/ui/lower-title"
 
 test("LowerTitleが表示されるか", () => {
   render(<LowerTitle title="名前" enTitle="name" />)
+  expect(screen.getByText("名前")).toBeVisible()
 })
--- __tests__/unit/timeline.test.tsx
+++ __tests__/unit/timeline.test.tsx
@@ -1,7 +1,11 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { test } from "vitest"
 import { Timeline } from "../../app/components/ui/timeline"
 
 test("Timelineが表示されるか", () => {
-  render(<Timeline title="名前" date="name" body="aa" />)
+  render(<Timeline title="高校入学" date="2024-01-01" body="高校入学の説明" />)
+
+  expect(screen.getByText("高校入学")).toBeVisible()
+  expect(screen.getByText("2024-01-01")).toBeVisible()
+  expect(screen.getByText("高校入学の説明")).toBeVisible()
 })
