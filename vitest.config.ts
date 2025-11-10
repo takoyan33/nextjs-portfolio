7bf8068f4335f6c23b921172c8decb8eef00e084
abeshumpei <harrier2070@gmail.com>
2025-11-10 20:01:50 +0900

    add: broswer modeの追加

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,10 @@
 /// <reference types="vitest" />
 import react from "@vitejs/plugin-react"
+import { playwright } from "@vitest/browser-playwright"
 import path from "path"
 import { defineConfig } from "vitest/config"
+
+const isBrowser = process.env.VITEST_BROWSER === "true"
 
 export default defineConfig({
   plugins: [react()],
@@ -23,6 +26,11 @@ export default defineConfig({
       deps: {
         inline: ["server-only"],
       },
+    },
+    browser: {
+      enabled: isBrowser,
+      provider: playwright(),
+      instances: [{ browser: "chromium" }],
     },
   },
 })
