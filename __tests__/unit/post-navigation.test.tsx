01a0bccde8f598e4ae0ae884657ba7cc4dff70a3
abeshumpei <harrier2070@gmail.com>
2025-11-20 23:16:51 +0900

    add: reviewの修正とテストケースの追加

--- __tests__/unit/post-navigation.test.tsx
+++ __tests__/unit/post-navigation.test.tsx
@@ -18,8 +18,23 @@ describe("PostNavigationの表示とリンク", () => {
     expect(screen.getByRole("link", { name: /次の記事/ })).toHaveAttribute("href", "/portfolios/2")
   })
 
+  test("正常系: リンクに遷移される", () => async () => {
+    render(
+      <PostNavigation
+        next_title="次の記事"
+        next_article_id="2"
+        prev_title="前の記事"
+        prev_article_id="1"
+      />,
+    )
+
+    await screen.getByRole("link", { name: /前の記事/ }).click()
+    expect(window.location.pathname).toBe("/portfolios/1")
+  })
+
   test("異常系: タイトルが空の場合はリンクが表示されない", () => {
     render(<PostNavigation />)
     expect(screen.queryByRole("link")).toBeNull()
+    expect(screen.queryByRole("span")).toBeNull()
   })
 })
