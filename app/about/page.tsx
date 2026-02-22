c7b87b919e42eb753881547f0449250d99b60a79
abeshumpei <harrier2070@gmail.com>
2026-02-22 20:58:03 +0900

    skillsの追加

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,20 +1,20 @@
-import { AboutTabs } from "@/app/about/_containers/about-tabs"
-import { Breadcrumb } from "@/components/ui/breadcrumb"
-import { LowerSubTitle } from "@/components/ui/lower-sub-title"
-import { LowerTitle } from "@/components/ui/lower-title"
-import { CareerHistoryTimeline } from "@/components/ui/rsc/career-history-timeline"
-import { JobTimeline } from "@/components/ui/rsc/job-timeline"
-import { LicenseList } from "@/components/ui/rsc/license-list"
-import "@/styles/page/_about.scss"
-import { PATH } from "@/utils/path"
-import type { Metadata } from "next"
-import { Suspense } from "react"
+import { AboutTabs } from "@/app/about/_containers/about-tabs";
+import { Breadcrumb } from "@/components/ui/breadcrumb";
+import { LowerSubTitle } from "@/components/ui/lower-sub-title";
+import { LowerTitle } from "@/components/ui/lower-title";
+import { CareerHistoryTimeline } from "@/components/ui/rsc/career-history-timeline";
+import { JobTimeline } from "@/components/ui/rsc/job-timeline";
+import { LicenseList } from "@/components/ui/rsc/license-list";
+import "@/styles/page/_about.scss";
+import { PATH } from "@/utils/path";
+import type { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "To You Design - About",
-}
+};
 
-export const dynamic = "force-static"
+export const dynamic = "force-static";
 
 const About = () => {
   return (
@@ -22,7 +22,7 @@ const About = () => {
       <div className="max_width">
         <Breadcrumb items={[{ name: "About", link: PATH.ABOUT }]} />
       </div>
-      <LowerTitle title="About" enTitle="プロフィール" />
+      <LowerTitle title="プロフィール" enTitle="About" />
       <AboutTabs>
         <Suspense fallback={<div>読み込み中...</div>}>
           <CareerHistoryTimeline />
@@ -40,7 +40,7 @@ const About = () => {
         </Suspense>
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default About
+export default About;
