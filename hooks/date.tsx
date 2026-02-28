650f840def6e017a253a740b10fda62114223d93
abeshumpei <harrier2070@gmail.com>
2026-02-28 17:06:48 +0900

    tzの修正

--- hooks/date.tsx
+++ hooks/date.tsx
@@ -1,5 +1,10 @@
-import dayjs from "dayjs"
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 export const formatDate = (date: string | Date): string => {
-  return dayjs(date).format("YYYY年MM年DD日")
-}
+  return dayjs(date).tz("Asia/Tokyo").format("YYYY年MM月DD日");
+};
