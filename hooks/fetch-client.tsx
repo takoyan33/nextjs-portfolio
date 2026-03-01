a5ea01807d9fbc666e51cfeea5c54aaa0144fa78
abeshumpei <harrier2070@gmail.com>
2026-03-01 23:01:37 +0900

    reviewの修正 formatの実行

--- hooks/fetch-client.tsx
+++ hooks/fetch-client.tsx
@@ -1,14 +1,11 @@
-import type { ResponsePortfolios } from "../types";
-import { CACHE_OPTIONS } from "../utils/data";
+import type { ResponsePortfolios } from "../types"
+import { CACHE_OPTIONS } from "../utils/data"
 
 /**
  * フロントエンドのポートフォリオの取得
  * @returns {ResponsePortfolios}
  */
 export const fetchPortfoliosFront = async (): Promise<ResponsePortfolios> => {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}api/portfolios`,
-    CACHE_OPTIONS
-  );
-  return await response.json();
-};
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}api/portfolios`, CACHE_OPTIONS)
+  return await response.json()
+}
