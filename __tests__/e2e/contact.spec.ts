d6fc34a9874b5ad55e644f1d479cfdeeb2cbf823
abeshumpei <harrier2070@gmail.com>
2026-03-29 15:56:45 +0900

    idの追加 login

--- __tests__/e2e/contact.spec.ts
+++ __tests__/e2e/contact.spec.ts
@@ -21,7 +21,8 @@ test("ローカル お問い合わせの実行", async ({ page }) => {
 
 test("本番環境 お問い合わせの実行", async ({ page }) => {
   await page.goto("https://to-you-design.vercel.app")
-  await page.getByRole("button", { name: "お問い合わせフォームへ" }).click()
+  await page.waitForTimeout(5000)
+  await page.getByRole("button", { name: "お問い合わせ" }).click()
   await page.waitForTimeout(1000)
   await page.getByRole("textbox", { name: "名前" }).click()
   await page.getByRole("textbox", { name: "名前" }).fill("test 太郎")
@@ -30,10 +31,12 @@ test("本番環境 お問い合わせの実行", async ({ page }) => {
   await page.getByRole("textbox", { name: "メッセージ" }).click()
   await page.getByRole("textbox", { name: "メッセージ" }).fill("test お問い合わせ")
   await page.getByRole("button", { name: "確認画面へ" }).click()
+  await page.waitForTimeout(2000)
+  await page.getByRole("button", { name: "送信する" }).click()
+  await page.waitForTimeout(5000)
   page.once("dialog", (dialog) => {
     console.log(`Dialog message: ${dialog.message()}`)
     dialog.dismiss().catch(() => {})
   })
-  await page.getByRole("button", { name: "送信する" }).click()
-  // mockのため送信確認まで行わない
+  await page.waitForURL("https://to-you-design.vercel.app/");
 })
---
+++ __tests__/e2e/login.spec.ts
@@ -0,0 +1,27 @@
+import { test } from "@playwright/test"
+
+test("ローカル ログインの実行", async ({ page }) => {
+  await page.goto("http://localhost:3000/admin")
+  await page.waitForTimeout(1000)
+  await page.getByRole("textbox", { name: "メールアドレス" }).click()
+  await page.getByRole("textbox", { name: "メールアドレス" }).fill("user@example.com")
+  await page.getByLabel("パスワード").click()
+  await page.getByLabel("パスワード").fill("password")
+  await page.getByRole("button", { name: "ログイン" }).click()
+  // mockのため送信確認まで行わない
+})
+
+test("本番環境 ログインの実行", async ({ page }) => {
+  await page.goto("https://to-you-design.vercel.app/admin")
+  await page.waitForTimeout(1000)
+  await page.getByRole("textbox", { name: "email" }).click()
+  await page.getByRole("textbox", { name: "email" }).fill("user@example.com")
+  await page.getByLabel("パスワード").click()
+  await page.getByLabel("パスワード").fill("password")
+  await page.getByRole("button", { name: "ログイン" }).click()
+  await page.waitForTimeout(3000)
+  await page.waitForURL("https://to-you-design.vercel.app/admin/dashboard");
+  await page.getByRole("button", { name: "ログアウト" }).click()
+  await page.waitForTimeout(3000)
+  await page.waitForURL("https://to-you-design.vercel.app");
+})
