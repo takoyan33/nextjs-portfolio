35c275b3c4f6eeca85d94a6cbf6973b4fb44c983
abeshumpei <harrier2070@gmail.com>
2026-05-03 18:13:03 +0900

    SSGに変更

--- app/portfolios/[id]/page.tsx
+++ app/portfolios/[id]/page.tsx
@@ -4,10 +4,7 @@ import "@/styles/page/_portfolioId.scss"
 import { logger } from "@/utils/logger"
 import { notFound } from "next/navigation"
 
-/**
- * 動的 [id] はオンデマンド／ISR で生成する
- */
-export const revalidate = 3600
+export const dynamic = "force-static"
 
 export default async function PortfolioPage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params
