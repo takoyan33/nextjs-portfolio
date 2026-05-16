89ad08286c5e9e7ba68843426481da3dee12814c
abeshumpei <harrier2070@gmail.com>
2026-05-16 14:08:04 +0900

    update: oxlint 並び替えの適用

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,9 @@
 /// <reference types="vitest" />
 import react from "@vitejs/plugin-react"
 import { playwright } from "@vitest/browser-playwright"
+
 import path from "path"
+
 import { defineConfig } from "vitest/config"
 
 export default defineConfig({
